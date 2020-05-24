bf13611369208311b5889cbed0663c65e6cb2c3b
Karla <karla.gardiner@gmail.com>
2020-05-24 11:56:58 +0100

    Error messages display when new blog form doesn't pass validation

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,4 +7,19 @@ class BlogsController < ApplicationController
   def index
   end
 
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      flash[:notice] = "Blog was successfully created"
+      redirect_to blog_path(@blog)
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def blog_params
+      params.require(:blog).permit(:title, :description)
+    end
+
 end
